97610db7f56e9db6f35321eecc0e628fb705dc72
essteer <essteer@pm.me>
2023-08-23 22:21:05 +0800

    add further examples

--- src/L11_Computational_Complexity/big_o_examples.py
+++ src/L11_Computational_Complexity/big_o_examples.py
@@ -18,7 +18,36 @@ def int_to_str(i):
     if i == "0":
         return "0"
     result = ""
+    # steps are constant within the loop, so question is how many times the loop is entered
     while i > 0:
         result = digits[i % 10] + result
         i = i//10
     return result
+
+
+# O(n) - linear complexity
+# e.g. searching a list in a sequence to check whether an element is present
+# adding characters of a string
+def add_digits(s):
+    val = 0  # constant
+    for c in s:  # number of times through loop is linear with len(s)
+        val += int(c)  # constant
+    return val  # constant
+
+
+# complexity can depend on number of recursive calls
+# iterative example below is slightly faster in time
+def fact_iter(n):
+    prod = i
+    for i in range(1, n+1):
+        prod *= i
+    return prod
+
+
+# number of function calls is linear in n, so still O(n)
+def fact_recur(n):
+    """asssume n >= 0"""
+    if n <= 1:
+        return 1
+    else:
+        return n * fact_recur(n - 1)
